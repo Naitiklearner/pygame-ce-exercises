34c5331169c0e0baf9dbe8eaad4c7976881122bd
Naitiklearner <ng4105864@gmail.com>
2025-01-02 10:49:51 +0530

    animation

--- 5-games-EX/vampire_survivor/Vampire-EX-3/code/main.py
+++ 5-games-EX/vampire_survivor/Vampire-EX-3/code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 from random import randint
 
@@ -15,13 +16,13 @@ class Game:
         self.running = True
 
         # groups 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
 
         # sprites
-        self.player = Player((500,300), self.all_sprites, self.collision_sprites)
+
 
 
     def setup(self):
@@ -35,6 +36,11 @@ class Game:
 
         for obj in map.get_layer_by_name('Collisions'):
             CollisionSprites((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+        
+        for marker in map.get_layer_by_name('Entities'):
+            if marker.name == 'Player':
+                self.player = Player((marker.x, marker.y), self.all_sprites, self.collision_sprites)
+
     def run(self):
         while self.running:
             # dt 
@@ -47,10 +53,9 @@ class Game:
 
             # update 
             self.all_sprites.update(dt)
-
             # draw
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             
             pygame.display.update()
 
